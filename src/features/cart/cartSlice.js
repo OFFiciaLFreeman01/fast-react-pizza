b47f7c210f4db2a1ac9934d1473182dd1e2c67c8
OFFiciaLFreeman01 <freeme9577@gmail.com>
2026-08-04 02:41:19 -0700

    Initial commit

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,137 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { createSelector } from 'reselect';
+
+const initialState = {
+  cart: [],
+
+  //   cart: [
+  //     {
+  //       pizzaId: 12,
+  //       meme: 'meditarrean',
+  //       quantity: 4,
+  //       unitPrice: 16,
+  //       totalPrice: 32,
+  //     },
+  //   ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      //payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = pizza
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item) {
+        item.quantity++;
+        item.totalPrice = item.quantity * item.unitPrice;
+      }
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item) {
+        item.quantity--;
+        item.totalPrice = item.quantity * item.unitPrice;
+      }
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+// import { createSlice } from '@reduxjs/toolkit';
+// import { createSelector } from 'reselect';
+
+// const initialState = {
+//   cart: [],
+
+//   //   cart: [
+//   //     {
+//   //       pizzaId: 12,
+//   //       meme: 'meditarrean',
+//   //       quantity: 4,
+//   //       unitPrice: 16,
+//   //       totalPrice: 32,
+//   //     },
+//   //   ],
+// };
+
+// const cartSlice = createSlice({
+//   name: 'cart',
+//   initialState,
+//   reducers: {
+//     addItem(state, action) {
+//       //payload = newItem
+//       state.cart.push(action.payload);
+//     },
+//     deleteItem(state, action) {
+//       // payload = pizza
+//       state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+//     },
+//     increaseItemQuantity(state, action) {
+//       const item = state.cart.find((item) => item.pizzaId === action.payload);
+//       if (item) {
+//         item.quantity++;
+//         item.totalPrice = item.quantity * item.unitPrice;
+//       }
+//     },
+//     decreaseItemQuantity(state, action) {
+//       const item = state.cart.find((item) => item.pizzaId === action.payload);
+//       if (item) {
+//         item.quantity--;
+//         item.totalPrice = item.quantity * item.unitPrice;
+//       }
+//     },
+//     clearCart(state) {
+//       state.cart = [];
+//     },
+//   },
+// });
+
+// export const {
+//   addItem,
+//   deleteItem,
+//   increaseItemQuantity,
+//   decreaseItemQuantity,
+//   clearCart,
+// } = cartSlice.actions;
+
+// export default cartSlice.reducer;
+
+// export const getCart = (state) => state.cart.cart;
+
+// export const getTotalCartQuantity = createSelector(
+//   (state) => state.cart.cart,
+//   (cart) => cart.reduce((sum, item) => sum + item.quantity, 0),
+// );
+
+// export const getTotalCartPrice = createSelector(
+//   (state) => state.cart.cart,
+//   (cart) => cart.reduce((sum, item) => sum + item.totalPrice, 0),
+// );
